e774ace54c158cf54b011f36220b027d3ea21543
Max Mastalerz <contact@maxmastalerz.com>
2021-05-18 23:23:54 -0400

    updated download controller to work with s3. updated menu overflow on small screens

--- strapi/api/download/controllers/download.js
+++ strapi/api/download/controllers/download.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const fs = require('fs');
+const axios = require('axios');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+
+async function get_stream_from_url(url) {
+	return new Promise((resolve) => {
+		axios({
+			method: 'get',
+			url: url,
+			responseType: 'stream'
+		})
+		.then(function (response) {
+			resolve(response.data);
+		});
+	});
+}
 
 module.exports = {
 	async findOne(ctx) {
@@ -14,12 +28,15 @@ module.exports = {
 		const entity = await strapi.services.download.findOne({ name });
 
 		if(entity) {
-			//const downloadLink = `/api${entity.download.url}`;
-			//ctx.redirect(downloadLink);
-			const downloadLink = `/api${entity.download.url}`;
-			ctx.body = fs.createReadStream(`${__dirname}/../../../public${entity.download.url}`);
-			ctx.set('Content-disposition', `attachment; filename=${entity.name}`);
-			ctx.set('Content-type', entity.download.mime);
+			if(entity.download.url[0] === '/') { //local file.
+				ctx.body = fs.createReadStream(`${__dirname}/../../../public${entity.download.url}`);
+				ctx.set('Content-disposition', `attachment; filename=${entity.name}`);
+				ctx.set('Content-type', entity.download.mime);
+			} else { //file stored in s3
+				ctx.body = await get_stream_from_url(entity.download.url);
+				ctx.set('Content-disposition', `attachment; filename=${entity.name}`);
+				ctx.set('Content-type', entity.download.mime);
+			}
 		}
 	},
 };
