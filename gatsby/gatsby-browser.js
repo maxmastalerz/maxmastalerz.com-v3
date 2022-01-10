cf4208620dba2e6560c433cde7a06277b8bf067f
Max Mastalerz <contact@maxmastalerz.com>
2022-01-10 02:53:24 -0500

    removing extra files. going back to use of npm. loading only certain parts of bootstrap. removing animate.css

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -5,8 +5,7 @@
  */
 
 // You can delete this file if you're not using it
-import './src/components/App/assets/css/bootstrap.min.css';
-import './src/components/App/assets/css/animate.min.css';
+import './src/components/App/assets/css/bootstrap-overrides.css';
 import './src/components/App/assets/css/boxicons.min.css';
 import './src/components/App/assets/css/style.css';
 import './src/components/App/assets/css/responsive.css';
