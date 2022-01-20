de1c153fb80ee81710817a94ea577c958d22b552
Max Mastalerz <contact@maxmastalerz.com>
2022-01-20 03:48:42 -0500

    trying out plugin

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -86,5 +86,21 @@ module.exports = {
         siteUrl: `${process.env.GATSBY_PROTOCOL}${process.env.GATSBY_BASE_URL}`,
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/api/*": [
+            "Host: maxmastalerz.com",
+          ]
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ]
 }
