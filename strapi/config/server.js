837346323c2fb8b7fc44136febdcef3b13fb28da
Max Mastalerz <contact@maxmastalerz.com>
2022-01-21 05:13:47 -0500

    fix up prod

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -3,6 +3,9 @@ module.exports = ({ env }) => {
 	const isDev = env.bool('IS_DEV', false);
 	const devPrefix = isDev ? 'dev.' : '';
 
+	console.log(`Launching strapi with NODE_ENV: ${env('NODE_ENV')} and IS_DEV: ${env.bool('IS_DEV')}`);
+	console.log(`Strapi URL: ${protocol}api.${devPrefix}maxmastalerz.com`);
+
 	return ({
 		host: env('HOST'),
 		port: env.int('PORT'),
