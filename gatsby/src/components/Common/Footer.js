5b01273a3e168e42ea1dea08c3e2ee23ee65189c
Max Mastalerz <contact@maxmastalerz.com>
2022-01-23 21:27:44 -0500

    width and height explicit. quote fixes

--- gatsby/src/components/Common/Footer.js
+++ gatsby/src/components/Common/Footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
                     <div className="copyright-item">
                         <p className="mb-3">&copy;{currentYear} <Link to="/">maxmastalerz.com</Link></p>
                         <a href="https://www.eff.org/pages/blue-ribbon-campaign" className="mx-2">
-                            <img src="https://www.eff.org/files/brstrip.gif" alt="Join the Blue Ribbon Online Free Speech Campaign" />
+                            <img src="https://www.eff.org/files/brstrip.gif" alt="Join the Blue Ribbon Online Free Speech Campaign" width="150" height="41"/>
                         </a>
-                        <a href='https://ipv6-test.com/validate.php?url=https://maxmastalerz.com' className="mx-2">
-                            <img src='https://ipv6-test.com/button-ipv6-small.png' alt='ipv6 ready' title='ipv6 ready' border='0' />
+                        <a href="https://ipv6-test.com/validate.php?url=https://maxmastalerz.com" className="mx-2">
+                            <img src="https://ipv6-test.com/button-ipv6-small.png" alt="ipv6 ready" title="ipv6 ready" border="0" width="88" height="31"/>
                         </a>
                     </div>
                 </div>
