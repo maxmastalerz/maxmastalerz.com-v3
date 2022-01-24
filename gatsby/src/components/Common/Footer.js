273e08cb4d906f4c35a7a00a8b4b381218d1768a
Max Mastalerz <contact@maxmastalerz.com>
2022-01-23 21:09:57 -0500

    Adding freedom

--- gatsby/src/components/Common/Footer.js
+++ gatsby/src/components/Common/Footer.js
@@ -8,7 +8,13 @@ const Footer = () => {
             <div className="copyright-area three">
                 <div className="container">
                     <div className="copyright-item">
-                        <p>&copy;{currentYear} <Link to="/">maxmastalerz.com</Link></p>
+                        <p className="mb-3">&copy;{currentYear} <Link to="/">maxmastalerz.com</Link></p>
+                        <a href="https://www.eff.org/pages/blue-ribbon-campaign" className="mx-2">
+                            <img src="https://www.eff.org/files/brstrip.gif" alt="Join the Blue Ribbon Online Free Speech Campaign" />
+                        </a>
+                        <a href='http://ipv6-test.com/validate.php?url=referer' className="mx-2">
+                            <img src='http://ipv6-test.com/button-ipv6-small.png' alt='ipv6 ready' title='ipv6 ready' border='0' />
+                        </a>
                     </div>
                 </div>
             </div>
