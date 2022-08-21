051f4c06c48bb55c5150211f8f2a3440c9214624
Max Mastalerz <contact@maxmastalerz.com>
2022-08-20 23:09:00 -0400

    Adding og:image to main page of website

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState, useRef } from "react"
-import Seo from "../components/App/seo"
-import Navbar from "../components/IndexPage/Navbar"
-import MainBanner from "../components/IndexPage/MainBanner"
-import About from "../components/IndexPage/About"
-import Services from "../components/IndexPage/Services"
-import Experience from "../components/IndexPage/Experience"
-import Projects from "../components/IndexPage/Projects"
-import FeaturedBlogPosts from "../components/IndexPage/FeaturedBlogPosts"
-import Testimonials from "../components/IndexPage/Testimonials"
-import ContactForm from "../components/IndexPage/ContactForm"
-import Footer from "../components/Common/Footer"
+import React, { useEffect, useState, useRef } from "react";
+import { Helmet } from "react-helmet";
+import Seo from "../components/App/seo";
+import Navbar from "../components/IndexPage/Navbar";
+import MainBanner from "../components/IndexPage/MainBanner";
+import About from "../components/IndexPage/About";
+import Services from "../components/IndexPage/Services";
+import Experience from "../components/IndexPage/Experience";
+import Projects from "../components/IndexPage/Projects";
+import FeaturedBlogPosts from "../components/IndexPage/FeaturedBlogPosts";
+import Testimonials from "../components/IndexPage/Testimonials";
+import ContactForm from "../components/IndexPage/ContactForm";
+import Footer from "../components/Common/Footer";
 
 import "../assets/styles/component-scope/IndexPage.scss";
 import "../assets/styles/component-scope/IndexPage.responsive.scss";
@@ -45,6 +46,14 @@ const IndexPage = ({data}) => {
         <div className="body-bg-three">
             <div className="main-area">
                 <Seo title="Home"/>
+                <Helmet
+                  meta={[
+                    {
+                      property: `og:image`,
+                      content: `https://daavgqhmwmui1.cloudfront.net/maxmastalerz_main_89b759c619.png`,
+                    },
+                  ]}
+                />
                 <Navbar setDisplayMasonryProjects={setDisplayMasonryProjects}/>
                 <div className="main-content">
                     <MainBanner />
